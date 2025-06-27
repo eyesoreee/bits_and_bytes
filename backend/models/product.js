f2266bd96f77467b5f0c39f6f8bb54a96a2f215c
Johaire Salih <salihjohaire@gmail.com>
2025-06-27 10:11:20 +0800

    added controller and routes for each model.

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Product extends Model {
     /**
@@ -10,24 +8,34 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Product.belongsTo(models.Category, {
+        foreignKey: "categoryId",
+        as: "category",
+      });
+      Product.belongsTo(models.Manufacturer, {
+        foreignKey: "manufacturerId",
+        as: "manufacturer",
+      });
     }
   }
-  Product.init({
-    name: DataTypes.STRING,
-    price: DataTypes.DECIMAL,
-    originalPrice: DataTypes.DECIMAL,
-    image: DataTypes.STRING,
-    rating: DataTypes.FLOAT,
-    reviews: DataTypes.INTEGER,
-    categoryId: DataTypes.INTEGER,
-    manufacturerId: DataTypes.INTEGER,
-    stock: DataTypes.INTEGER,
-    description: DataTypes.TEXT,
-    isSale: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'Product',
-  });
+  Product.init(
+    {
+      name: DataTypes.STRING,
+      price: DataTypes.DECIMAL,
+      originalPrice: DataTypes.DECIMAL,
+      image: DataTypes.STRING,
+      rating: DataTypes.FLOAT,
+      reviews: DataTypes.INTEGER,
+      categoryId: DataTypes.INTEGER,
+      manufacturerId: DataTypes.INTEGER,
+      stock: DataTypes.INTEGER,
+      description: DataTypes.TEXT,
+      isSale: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: "Product",
+    }
+  );
   return Product;
 };
